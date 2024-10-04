54c3da071a2e8f4df41afae81e0be02d39e58e11
Kenny Le <kennyle1412@gmail.com>
2024-10-03 22:43:29 -0500

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-sudo apt install zsh build-essential unzip pipx -y
+sudo apt install zsh build-essential unzip pipx python3-pip -y
 
 if ! command -v brew &>/dev/null; then
   echo "Homebrew is not installed"
@@ -17,25 +17,13 @@ if ! grep -q 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' "$HOME/.zs
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
 
-brew install fzf zoxide ripgrep fd starship gh tmux node lazygit git-branchless
+brew install fzf zoxide ripgrep fd starship gh tmux node lazygit git-branchless go zig
 
 current_path=$(dirname $(realpath "$0"))
-[ ! -L "$HOME/.tmux.conf" ] && ln -s $current_path/.tmux.conf ~/.tmux.conf
-[ ! -L "$HOME/.zshrc" ] && ln -s $current_path/.zshrc ~/.zshrc
-
+[ ! -L "$HOME/.tmux.conf" ] && curl -fsSL https://raw.githubusercontent.com/knyhle/dotfiles/refs/heads/main/.tmux.conf -o $HOME/.tmux.conf
+[ ! -L "$HOME/.zshrc" ] && ln -s curl -fsSL https://raw.githubusercontent.com/knyhle/dotfiles/refs/heads/main/.zshrc -o $HOME/.zshrc
 [ ! -d "$HOME/.tmux/plugins/tpm" ] && git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ~/.tmux/plugins/tpm/bin/install_plugins
 
-if ! command -v go version &>/dev/null; then
-  wget https://go.dev/dl/go1.23.0.linux-amd64.tar.gz
-  sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.23.0.linux-amd64.tar.gz
-  rm go1.23.0.linux-amd64.tar.gz
-fi
-
-if ! grep -q 'export PATH=$PATH:/usr/local/go/bin' "$HOME/.profile"; then
-  echo 'export PATH=$PATH:/usr/local/go/bin' >>"$HOME/.profile"
-  source "$HOME/.profile"
-fi
-
-pipx install poetry
 pipx install --include-deps ansible
+pip install uv
